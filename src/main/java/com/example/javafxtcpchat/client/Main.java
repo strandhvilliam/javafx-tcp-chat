0b2712b06dd65bcbebaad1ecdc8fd23e91a5002b
Villiam Strandh <strandh.villiam@gmail.com>
2022-11-15 13:41:18 +0100

    main update

--- src/main/java/com/example/javafxtcpchat/client/Main.java
+++ src/main/java/com/example/javafxtcpchat/client/Main.java
@@ -1,9 +1,18 @@
 package com.example.javafxtcpchat.client;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,14 +21,31 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("chatroom.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Chat!");
+
+        ChatController controller = fxmlLoader.getController();
+        ToolBar t = controller.chatToolBar;
+        Delta dragDelta = new Delta();
+        t.setOnMousePressed(e -> {
+            dragDelta.x = stage.getX() - e.getScreenX();
+            dragDelta.y = stage.getY() - e.getScreenY();
+        });
+        t.setOnMouseDragged(e -> {
+            stage.setX(e.getScreenX() + dragDelta.x);
+            stage.setY(e.getScreenY() + dragDelta.y);
+        });
+
         stage.setScene(scene);
-
         stage.show();
-
     }
+
+
 
     public static void main(String[] args) {
         launch();
     }
+
+    class Delta {double x, y;}
+
 }
